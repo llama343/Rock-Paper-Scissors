8809316d7c6775685f3dd82499bead8c59ff96ee
llama343 <dj_schilling@msn.com>
2022-07-11 14:38:29 -0500

    Add index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
 //Get a random floating point value, return rock, paper, or scissors
-
 function computerPlay() {
   let number = Math.random();
     if (number <= .333333) {
@@ -14,8 +13,23 @@ function computerPlay() {
 }
 
 
-console.log(computerPlay());
 
+
+
+//playing 1 round, displaying if win or lose
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'rock' && computerSelection === 'Scissors!') {
+        console.log('You win!');
+    } else {
+        console.log('You lose');
+    }
+    
+}
+
+const playerSelection ='rock';
+const computerSelection = computerPlay();
+
+console.log(playRound('rock', computerSelection));
 
 
 
