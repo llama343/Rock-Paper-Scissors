5768913cee1a6eb05d58aed01ceb9deb837f7036
llama343 <dj_schilling@msn.com>
2022-07-13 15:23:54 -0500

    Add index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-//get user input 'rock, paper, scissors' / store answer in "const answer"
+let score = 0;
+let playerScore = 0;
+let computerScore = 0;
+let playerWinRound = "Player wins this round!"
+let computerWinRound = "Computer wins this round!"
+let draw = "Draw!"
+let playerWin = "Player wins the game! Congratulations!"
+let computerWin = "Computer wins the game! Congratulations!"
 
-function getInput() {
-    return prompt('Please select rock, paper, or scissors').toLowerCase();
-    
+
+function playerChoice() {
+   return prompt('Please chose rock, paper, or scissors').toLowerCase();
 }
-//storing userInput
-const answer = getInput();
+let answer = playerChoice();
 console.log(answer);
 
 
@@ -24,29 +30,42 @@ function computerPlay() {
     
 }
 //storing computerPlay
-const computerAnswer = computerPlay()
+let computerAnswer = computerPlay();
 console.log(computerAnswer);
+
 
 //playing one round
 function playRound(answer, computerAnswer) {
     if (answer === 'rock' && computerAnswer === 'scissors') {
-        return ('You win! Rock beats scissors');
+        return playerWinRound;
     } else if(answer === 'paper' && computerAnswer === 'rock') {
-        return ('You win! Paper beats rock');
+        return playerWinRound;
     } else if (answer === 'scissors' && computerAnswer === 'paper') {
-        return ('You win! Scissors beats paper');
+        return playerWinRound;
     } else if (answer === computerAnswer) {
-        return ('You tie! You chose the same thing!!');
+        return draw;
     } else {
-        return ('You lose! Try again');
+        return computerWinRound;
     }
     
 }
+//storing result of 1 round
+let roundResult = playRound(answer, computerAnswer);
+console.log(roundResult);
 
-const oneRound = playRound(answer, computerAnswer);
-console.log (playRound(answer, computerAnswer));
 
 
+
+for (let i = 0; i < 1000; i++) {
+    let answer = playerChoice();
+    console.log(answer);
+    let computerAnswer = computerPlay();
+    console.log(computerAnswer);
+    let roundResult = playRound(answer, computerAnswer);
+    console.log(roundResult);
+
+     
+}
 
 
 
