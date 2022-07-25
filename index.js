b1d2d591931cee97e930fe3a0a47a59533a0c972
llama343 <dj_schilling@msn.com>
2022-07-25 10:57:18 -0500

    Add index.html index.js style.css

--- index.js
+++ index.js
@@ -1,99 +1,73 @@
-let draws = 0;
+
+const buttons = document.querySelectorAll('input')
+const resetBtn = document.querySelector('#reset');
+
+
+resetBtn.addEventListener('click',() => location.reload());
+
+
+
+
+
+//original java
+
 let playerScore = 0;
 let computerScore = 0;
-let playerWinRound = "Player wins this round!"
-let computerWinRound = "Computer wins this round!"
-let draw = "Draw!"
-let playerWin = "Player wins the game! Congratulations!"
-let computerWin = "Computer wins the game! Congratulations!"
-
-
-function playerChoice() {
-   return prompt('Please chose rock, paper, or scissors').toLowerCase();
-}
-let answer = playerChoice();
-console.log(answer);
 
 
 
 
-//Get a random floating point value, return rock, paper, or scissors
+ 
+
+
+
+
+
+//Get a random computer choice
 function computerPlay() {
-  let number = Math.random();
-    if (number <= .333333) {
-        return ('rock');
-    } else if (number <= .66666) {
-        return ('paper');
-    } else  {
-        return ('scissors');
-    }   
-    
+  let choices = ['rock', 'paper', 'scissors']
+  return choices[Math.floor(Math.random() * choices.length)]
 }
-//storing computerPlay
-let computerAnswer = computerPlay();
-console.log(computerAnswer);
+
+
+
 
 
 //playing one round
-function playRound(answer, computerAnswer) {
-    if (answer === 'rock' && computerAnswer === 'scissors') {
-        return playerWinRound;
-    } else if(answer === 'paper' && computerAnswer === 'rock') {
-        return playerWinRound;
-    } else if (answer === 'scissors' && computerAnswer === 'paper') {
-        return playerWinRound;
-    } else if (answer === computerAnswer) {
-        return draw;
-    } else {
-        return computerWinRound;
-    }
+function playRound(playerSelection) {
+
+    let computerSelection = computerPlay()
+    let result = ''
+
+    if ((playerSelection == 'rock' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')||
+        (playerSelection == 'scissors' && computerSelection == 'paper')) {
+            playerScore += 1
+            result = (' You win! ' + playerSelection + ' beats ' + computerSelection
+                + '<br><br>Player score: ' + playerScore + '<br>Computer Score: ' + computerScore)
+
+                if (playerScore === 5) {
+                    result += '<br><br>You won the game!'
+                }   
+        } else if (playerSelection == computerSelection) {
+            result = ('it\'s a tie. You both chose ' + playerSelection
+            + '<br><br>Player score: ' + playerScore + '<br>Computer Score: ' + computerScore)
+        } else {
+            computerScore += 1
+            result = (' You lose! ' + computerSelection + ' beats ' + playerSelection
+            + '<br><br>Player score: ' + playerScore + '<br>Computer Score: ' + computerScore)
+
+            if (computerScore == 5) {
+                result += 'I won the game!'
+            }
+        }
+        document.getElementById('result').innerHTML = result 
+        return
     
 }
-//storing result of 1 round
-let roundResult = playRound(answer, computerAnswer);
-console.log(roundResult);
 
-console.log ('Your score is ' + playerScore);
-console.log('The computer score is ' + computerScore);
-
-
-
-//for loop plays mutliple rounds
-for (let i = 0; i < 1000; i++) {
-    let answer = playerChoice();
-    console.log(answer);
-    let computerAnswer = computerPlay();
-    console.log(computerAnswer);
-    let roundResult = playRound(answer, computerAnswer);
-    console.log(roundResult);
-    gameScore(roundResult);
-    console.log ('Your score is ' + playerScore);
-    console.log('The computer score is ' + computerScore);
-
-    if (playerScore === 5 || computerScore === 5) {
-        break;
-    }
-
-     
-}
-
-//adds points
-function gameScore(roundResult) {
-    if (roundResult === playerWinRound) {
-        playerScore++;
-    } else if (roundResult === draw) {
-        draws++;
-    } else {
-        computerScore++;
-    }
-//stops game if score is reached
-    if (playerScore === 5) {
-        console.log(playerWin);
-        return;
-    }
-
-    if (computerScore === 5) {
-        console.log(computerWin);
-        return;
-    }
-}
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+        playRound(button.value)
+    })
+})
